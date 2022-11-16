54367beff1725906f2a07baa4b40eac545de3beb
Alexandre Belisário Duarte Leite de Andrade <alexbdla@gmail.com>
2022-11-16 13:42:11 -0300

    Exercise finished!

---
+++ Bank/Entities/SavingsAccount.cs
@@ -0,0 +1,27 @@
+﻿namespace Bank.Entities
+{
+    internal class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate)
+            : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance *= (1 + (InterestRate / 100));
+        }
+
+        public sealed override void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+    }
+}
